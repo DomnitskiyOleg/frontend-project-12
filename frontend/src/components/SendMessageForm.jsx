eee70f7fe19bd8b627e558bb0bc999f3aa958952
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-16 12:27:10 +0600

    add form for sending messages

---
+++ frontend/src/components/SendMessageForm.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { useFormik } from 'formik';
+
+const SendMessageForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      message: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <div className="mt-auto py-3 px-5">
+      <Form noValidate="" className="py-1 border rounded-2">
+        <Form.Group className="input-group has-validation">
+          <Form.Control
+            name="message"
+            aria-label="Новое сообщение"
+            placeholder="Введите сообщение..."
+            className="border-0 p-1 form-control"
+            onChange={formik.handleChange}
+            value={formik.values.message}
+          />
+          <button type="submit" className="btn btn-group-vertical" disabled="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              width="20"
+              height="20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+              />
+            </svg>
+            <span className="visually-hidden">Отправить</span>
+          </button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+export default SendMessageForm;
